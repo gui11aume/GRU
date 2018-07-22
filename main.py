1148db7554a3e624377d63d769c8b1e1a4866801
gui11aume <guillaume.filion@gmail.com>
2018-07-22 23:15:29 +0200

    Rewrote everything with OOP.

--- main.py
+++ main.py
@@ -1,212 +1,305 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
 import numpy as np
+import sys
 
-# Seed random
-np.random.seed(0)
 
-# Read data and setup maps for integer encoding and decoding.
-data = open('input.txt', 'r').read()
-chars = sorted(list(set(data))) # Sort makes model predictable (if seeded).
-data_size, vocab_size = len(data), len(chars)
-print('data has %d characters, %d unique.' % (data_size, vocab_size))
-char_to_ix = { ch:i for i,ch in enumerate(chars) }
-ix_to_char = { i:ch for i,ch in enumerate(chars) }
+def onehot(sz, pos):
+   o = np.zeros(sz); o[pos] = 1
+   return o
 
-# Activation functions
-# NOTE: Derivatives are calculated using outcomes of their primitives (which are already calculated during forward prop).
-def sigmoid(input, deriv=False):
-    if deriv:
-        return input*(1-input)
-    else:
-        return 1 / (1 + np.exp(-input))
 
-def tanh(input, deriv=False):
-    if deriv:
-        return 1 - input ** 2
-    else:
-        return np.tanh(input)
+class loss:
+   @staticmethod
+   def fn(x,y):
+      return - np.log(np.dot(softmax.fn(x), y))
+   @staticmethod
+   def dx(x,y):
+      return softmax.fn(x) - y
+      
+class softmax:
+   @staticmethod
+   def fn(x):
+      e = np.exp(x - np.max(x))
+      return e / e.sum()
 
-# Derivative is directly calculated in backprop (in combination with cross-entropy loss function).
-def softmax(input):
-    # Subtraction of max value improves numerical stability.
-    e_input = np.exp(input - np.max(input))
-    return e_input / e_input.sum()
+class sigmoid:
+   @staticmethod
+   def fn(x):
+      return 1 / (1 + np.exp(-x))
+   @staticmethod
+   def df(x):
+      return x * (1-x)
 
-# Hyper parameters
-N, h_size, o_size = vocab_size, vocab_size, vocab_size # Hidden size is set to vocab_size, assuming that level of abstractness is approximately proportional to vocab_size (but can be set to any other value).
-seq_length = 25 # Longer sequence lengths allow for lengthier latent dependencies to be trained.
-learning_rate = 1e-1
+class tanh:
+   @staticmethod
+   def fn(x):
+      return np.tanh(x)
+   @staticmethod
+   def df(x):
+      return 1 - x**2
 
-# Model parameter initialization
-Wz = np.random.rand(h_size, N) * 0.1 - 0.05
-Uz = np.random.rand(h_size, h_size) * 0.1 - 0.05
-bz = np.zeros((h_size, 1))
 
-Wr = np.random.rand(h_size, N) * 0.1 - 0.05
-Ur = np.random.rand(h_size, h_size) * 0.1 - 0.05
-br = np.zeros((h_size, 1))
+class GRULayer:
 
-Wh = np.random.rand(h_size, N) * 0.1 - 0.05
-Uh = np.random.rand(h_size, h_size) * 0.1 - 0.05
-bh = np.zeros((h_size, 1))
+   def __init__(self, xsz, hsz, ysz):
+      # Dimensions.
+      self.xsz = xsz
+      self.hsz = hsz
+      self.ysz = ysz
 
-Wy = np.random.rand(o_size, h_size) * 0.1 - 0.05
-by = np.zeros((o_size, 1))
+      # Hidden states through time.
+      self.x  = list()           # Input vector.
+      self.z  = list()           # Update gate.
+      self.r  = list()           # Reset gate.
+      self.hh = list()           # Hidden intermediate.
+      self.h  = [np.zeros(hsz)]  # Hidden state.
+      self.y  = list()           # Outputs.
 
-def lossFun(inputs, targets, hprev):
-    # Initialize variables
-    x, z, r, h_hat, h, y, p = {}, {}, {}, {}, {-1: hprev}, {}, {} # Dictionaries contain variables for each timestep.
-    sequence_loss = 0
+      # Parameters.
+      self.Wz = np.random.rand(hsz, xsz) * 0.1 - 0.05
+      self.Uz = np.random.rand(hsz, hsz) * 0.1 - 0.05
+      self.bz = np.zeros(hsz) 
 
-    # Forward prop
-    for t in range(len(inputs)):
-        # Set up one-hot encoded input
-        x[t] = np.zeros((vocab_size, 1))
-        x[t][inputs[t]] = 1
-        
-        # Calculate update and reset gates
-        z[t] = sigmoid(np.dot(Wz, x[t]) + np.dot(Uz, h[t-1]) + bz)
-        r[t] = sigmoid(np.dot(Wr, x[t]) + np.dot(Ur, h[t-1]) + br)
-        
-        # Calculate hidden units
-        h_hat[t] = tanh(np.dot(Wh, x[t]) + np.dot(Uh, np.multiply(r[t], h[t-1])) + bh)
-        h[t] = np.multiply(z[t], h[t-1]) + np.multiply((1 - z[t]), h_hat[t])
-        
-        # Regular output unit
-        y[t] = np.dot(Wy, h[t]) + by
-        
-        # Probability distribution
-        p[t] = softmax(y[t])
-        
-        # Cross-entropy loss
-        loss = -np.sum(np.log(p[t][targets[t]]))
-        sequence_loss += loss
+      self.Wr = np.random.rand(hsz, xsz) * 0.1 - 0.05
+      self.Ur = np.random.rand(hsz, hsz) * 0.1 - 0.05
+      self.br = np.zeros(hsz) 
 
-    # Parameter gradient initialization
-    dWy, dWh, dWr, dWz = np.zeros_like(Wy), np.zeros_like(Wh), np.zeros_like(Wr), np.zeros_like(Wz)
-    dUh, dUr, dUz = np.zeros_like(Uh), np.zeros_like(Ur), np.zeros_like(Uz)
-    dby, dbh, dbr, dbz = np.zeros_like(by), np.zeros_like(bh), np.zeros_like(br), np.zeros_like(bz)
-    dhnext = np.zeros_like(h[0])
-    
-    # Backward prop
-    for t in reversed(range(len(inputs))):
-        # ∂loss/∂y
-        dy = np.copy(p[t])
-        dy[targets[t]] -= 1
-        
-        # ∂loss/∂Wy and ∂loss/∂by
-        dWy += np.dot(dy, h[t].T)
-        dby += dy
-        
-        # Intermediary derivatives
-        dh = np.dot(Wy.T, dy) + dhnext
-        dh_hat = np.multiply(dh, (1 - z[t]))
-        dh_hat_l = dh_hat * tanh(h_hat[t], deriv=True)
-        
-        # ∂loss/∂Wh, ∂loss/∂Uh and ∂loss/∂bh
-        dWh += np.dot(dh_hat_l, x[t].T)
-        dUh += np.dot(dh_hat_l, np.multiply(r[t], h[t-1]).T)
-        dbh += dh_hat_l
-        
-        # Intermediary derivatives
-        drhp = np.dot(Uh.T, dh_hat_l)
-        dr = np.multiply(drhp, h[t-1])
-        dr_l = dr * sigmoid(r[t], deriv=True)
-        
-        # ∂loss/∂Wr, ∂loss/∂Ur and ∂loss/∂br
-        dWr += np.dot(dr_l, x[t].T)
-        dUr += np.dot(dr_l, h[t-1].T)
-        dbr += dr_l
-        
-        # Intermediary derivatives
-        dz = np.multiply(dh, h[t-1] - h_hat[t])
-        dz_l = dz * sigmoid(z[t], deriv=True)
-        
-        # ∂loss/∂Wz, ∂loss/∂Uz and ∂loss/∂bz
-        dWz += np.dot(dz_l, x[t].T)
-        dUz += np.dot(dz_l, h[t-1].T)
-        dbz += dz_l
-        
-        # All influences of previous layer to loss
-        dh_fz_inner = np.dot(Uz.T, dz_l)
-        dh_fz = np.multiply(dh, z[t])
-        dh_fhh = np.multiply(drhp, r[t])
-        dh_fr = np.dot(Ur.T, dr_l)
-        
-        # ∂loss/∂h𝑡₋₁
-        dhnext = dh_fz_inner + dh_fz + dh_fhh + dh_fr
+      self.Wh = np.random.rand(hsz, xsz) * 0.1 - 0.05
+      self.Uh = np.random.rand(hsz, hsz) * 0.1 - 0.05
+      self.bh = np.zeros(hsz) 
 
-    return sequence_loss, dWy, dWh, dWr, dWz, dUh, dUr, dUz, dby, dbh, dbr, dbz, h[len(inputs) - 1]
+      self.Wy = np.random.rand(ysz, hsz) * 0.1 - 0.05
+      self.by = np.zeros(ysz) 
 
-def sample(h, seed_ix, n):
-    # Initialize first word of sample ('seed') as one-hot encoded vector.
-    x = np.zeros((vocab_size, 1))
-    x[seed_ix] = 1
-    ixes = [seed_ix]
-    
-    for t in range(n):
-        # Calculate update and reset gates
-        z = sigmoid(np.dot(Wz, x) + np.dot(Uz, h) + bz)
-        r = sigmoid(np.dot(Wr, x) + np.dot(Ur, h) + br)
-        
-        # Calculate hidden units
-        h_hat = tanh(np.dot(Wh, x) + np.dot(Uh, np.multiply(r, h)) + bh)
-        h = np.multiply(z, h) + np.multiply((1 - z), h_hat)
-        
-        # Regular output unit
-        y = np.dot(Wy, h) + by
-        
-        # Probability distribution
-        p = softmax(y)
+      # Gradients.
+      self.zerograd()
+      self.dhn = np.zeros(self.hsz)
 
-        # Choose next char according to the distribution
-        ix = np.random.choice(range(vocab_size), p=p.ravel())
-        x = np.zeros((vocab_size, 1))
-        x[ix] = 1
-        ixes.append(ix)
-    
-    return ixes
+      # Adagrad parameters.
+      self.mdWz = np.zeros_like(self.Wz)
+      self.mdWr = np.zeros_like(self.Wr)
+      self.mdWh = np.zeros_like(self.Wh)
+      self.mdWy = np.zeros_like(self.Wy)
 
-# Initialize sampling parameters and memory gradients (for adagrad)
-n, p = 0, 0
-mdWy, mdWh, mdWr, mdWz = np.zeros_like(Wy), np.zeros_like(Wh), np.zeros_like(Wr), np.zeros_like(Wz)
-mdUh, mdUr, mdUz = np.zeros_like(Uh), np.zeros_like(Ur), np.zeros_like(Uz)
-mdby, mdbh, mdbr, mdbz = np.zeros_like(by), np.zeros_like(bh), np.zeros_like(br), np.zeros_like(bz)
-smooth_loss = -np.log(1.0/vocab_size)*seq_length
+      self.mdUz = np.zeros_like(self.Uz)
+      self.mdUr = np.zeros_like(self.Ur)
+      self.mdUh = np.zeros_like(self.Uh)
 
-print_interval = 100
+      self.mdbz = np.zeros_like(self.bz)
+      self.mdbr = np.zeros_like(self.br)
+      self.mdbh = np.zeros_like(self.bh)
+      self.mdby = np.zeros_like(self.by)
 
-while True:
-    # Reset memory if appropriate
-    if p + seq_length + 1 >= len(data) or n == 0:
-        hprev = np.zeros((h_size, 1))
-        p = 0
-    
-    # Get input and target sequence
-    inputs = [char_to_ix[ch] for ch in data[p:p+seq_length]]
-    targets = [char_to_ix[ch] for ch in data[p+1:p+seq_length+1]]
 
-    # Occasionally sample from model and print result
-    if n % print_interval == 0:
-        sample_ix = sample(hprev, inputs[0], 1000)
-        txt = ''.join(ix_to_char[ix] for ix in sample_ix)
-        print('----\n%s\n----' % (txt, ))
+   def flush(self):
+      self.h  = [np.zeros(self.hsz)]
+      self.dhn = np.zeros(self.hsz)
 
-    # Get gradients for current model based on input and target sequences
-    loss, dWy, dWh, dWr, dWz, dUh, dUr, dUz, dby, dbh, dbr, dbz, hprev = lossFun(inputs, targets, hprev)
-    smooth_loss = smooth_loss * 0.999 + loss * 0.001
 
-    # Occasionally print loss information
-    if n % print_interval == 0:
-        print('iter %d, loss: %f, smooth loss: %f' % (n, loss, smooth_loss))
+   def zerograd(self):
+      self.dWz = np.zeros_like(self.Wz)
+      self.dWr = np.zeros_like(self.Wr)
+      self.dWh = np.zeros_like(self.Wh)
+      self.dWy = np.zeros_like(self.Wy)
 
-    # Update model with adagrad (stochastic) gradient descent
-    for param, dparam, mem in zip([Wy,  Wh,  Wr,  Wz,  Uh,  Ur,  Uz,  by,  bh,  br,  bz],
-                                  [dWy, dWh, dWr, dWz, dUh, dUr, dUz, dby, dbh, dbr, dbz],
-                                  [mdWy,mdWh,mdWr,mdWz,mdUh,mdUr,mdUz,mdby,mdbh,mdbr,mdbz]):
-        np.clip(dparam, -5, 5, out=dparam)
-        mem += dparam * dparam
-        param += -learning_rate * dparam / np.sqrt(mem + 1e-8) # Small added term for numerical stability
+      self.dUz = np.zeros_like(self.Uz)
+      self.dUr = np.zeros_like(self.Ur)
+      self.dUh = np.zeros_like(self.Uh)
 
-    # Prepare for next iteration
-    p += seq_length
-    n += 1
+      self.dbz = np.zeros_like(self.bz)
+      self.dbr = np.zeros_like(self.br)
+      self.dbh = np.zeros_like(self.bh)
+      self.dby = np.zeros_like(self.by)
+
+
+   def get_state(self):
+      return self.h[:]
+
+
+   def set_state(self, h):
+      self.h = h
+
+
+   def forward(self, x):
+      # Update and reset gates.
+      z = sigmoid.fn(np.dot(self.Wz, x) + \
+            np.dot(self.Uz, self.h[-1]) + self.bz)
+      r = sigmoid.fn(np.dot(self.Wr, x) + \
+            np.dot(self.Ur, self.h[-1]) + self.br)
+
+      # GRU units.
+      hh = tanh.fn(np.dot(self.Wh, x) + \
+            np.dot(self.Uh, np.multiply(r, self.h[-1])) + self.bh)
+      h = np.multiply(z, self.h[-1]) + np.multiply((1-z), hh)
+      y = np.dot(self.Wy, h) + self.by
+
+      # Write internal states.
+      self.x.append(x)
+      self.z.append(z)
+      self.r.append(r)
+      self.hh.append(hh)
+      self.y.append(y)
+      self.h.append(h)
+
+      return y
+
+
+   def backprop(self, dy):
+      '''The parameter 'dy' must be a list of gradients fed
+      into the GRU layer.'''
+
+      self.zerograd()
+      dhn = np.zeros(self.hsz)
+
+      T = len(dy)
+      dx = list()
+
+      for t in range(T-1,-1,-1):
+         # Layer output.
+         self.dby += dy[t]
+         self.dWy += np.outer(dy[t], self.h[t+1])
+
+         # Intermediates.
+         dh   = np.dot(self.Wy.T, dy[t]) + dhn
+         dhh  = np.multiply(dh, (1-self.z[t]))
+         dhhl = np.multiply(dhh, tanh.df(self.hh[t]))
+
+         # Gradient for 'hhat' parameters.
+         self.dWh += np.outer(dhhl, self.x[t])
+         self.dUh += np.outer(dhhl, np.multiply(self.r[t], self.h[t]))
+         self.dbh += dhhl
+
+         # Intermediates
+         drh = np.dot(self.Uh.T, dhhl)
+         dr  = np.multiply(drh, self.h[t])
+         drl = dr * sigmoid.df(self.r[t])
+
+         # Gradient for 'r' parameters.
+         self.dWr += np.outer(drl, self.x[t])
+         self.dUr += np.outer(drl, self.h[t])
+         self.dbr += drl
+
+         # Intermediates
+         dz  = np.multiply(dh, self.h[t] - self.hh[t])
+         dzl = dz * sigmoid.df(self.z[t])
+
+         # Gradient for 'z' parameters.
+         self.dWz += np.outer(dzl, self.x[t])
+         self.dUz += np.outer(dzl, self.h[t].T)
+         self.dbz += dzl
+
+         # Gradient for next 'h'
+         dh1 = np.dot(self.Uz.T, dzl)
+         dh2 = np.dot(self.Ur.T, drl)
+         dh3 = np.multiply(dh, self.z[t])
+         dh4 = np.multiply(drh, self.r[t])
+
+         dhn = dh1 + dh2 + dh3 + dh4
+
+         # Send down the gradient.
+         dxr = np.dot(self.Wr.T, drl)
+         dxz = np.dot(self.Wz.T, dzl)
+         dx.append(dxr+dxz)
+
+      # Erase internal variables.
+      self.x  = list()
+      self.z  = list()
+      self.r  = list()
+      self.hh = list()
+      self.h  = self.h[-1:]
+      self.y  = list()
+
+      return dx
+
+
+   def update(self, learning_rate=0.1):
+      # Update model with adagrad (stochastic) gradient descent
+      plist = [self.Wy, self.Wh, self.Wr, self.Wz, self.Uh,
+            self.Ur, self.Uz, self.by, self.bh, self.br, self.bz]
+      dlist = [self.dWy, self.dWh, self.dWr, self.dWz, self.dUh,
+            self.dUr, self.dUz, self.dby, self.dbh, self.dbr, self.dbz]
+      mlist = [self.mdWy, self.mdWh, self.mdWr, self.mdWz, self.mdUh,
+            self.mdUr, self.mdUz, self.mdby, self.mdbh, self.mdbr, self.mdbz]
+      for i in range(len(plist)):
+          np.clip(dlist[i], -5, 5, out=dlist[i])
+          mlist[i] += dlist[i] * dlist[i]
+          # Add small term for numerical stability.
+          plist[i] += -learning_rate * dlist[i] / np.sqrt(mlist[i] + 1e-8)
+      return
+
+
+def sample(RNN, x):
+   charidx = list()
+   intlstate = RNN.get_state()
+   for t in range(1000):
+      p = softmax.fn(RNN.forward(x))
+      # Choose next char according to the distribution
+      idx = np.random.choice(range(len(x)), p=p.ravel())
+      x = np.zeros_like(x)
+      x[idx] = 1
+      charidx.append(idx)
+   RNN.set_state(intlstate)
+   return charidx
+
+
+
+def main(fname):
+   '''Read data from an input text and train a GRU.'''
+   
+   np.random.seed(123)
+
+   with open(fname) as f: 
+      txt = f.read()
+
+   alphabet = sorted(list(set(txt)))
+   txtlen, sigma = len(txt), len(alphabet)
+   print('data has %d characters, %d unique.' % (txtlen, sigma))
+   char_to_idx = { ch:i for i,ch in enumerate(alphabet) }
+   idx_to_char = { i:ch for i,ch in enumerate(alphabet) }
+
+   hsz = sigma
+   G = GRULayer(sigma, hsz, sigma)
+
+   seq_length = 25
+   Si = range(seq_length)
+   learning_rate = 1e-1
+   print_interval = 1000
+
+   p = 1
+   n = 0
+   seq = [onehot(sigma, char_to_idx[txt[0]])]
+   while True:
+      # Check if end of text is reached.
+      if p + seq_length > txtlen:
+         G.flush()
+         seq = [onehot(sigma, char_to_idx[txt[0]])]
+         
+      # Get input and target sequence
+      for i in range(p, p+seq_length):
+         seq.append(onehot(sigma, char_to_idx[txt[i]]))
+
+      out =  [G.forward(seq[i]) for i in Si]
+      losses = sum([loss.fn(out[i], seq[i+1]) for i in Si])
+      G.backprop([loss.dx(out[i], seq[i+1]) for i in Si])
+      G.update()
+
+      # Keep last character for next round.
+      seq = seq[-1:]
+
+      if n % print_interval == 0:
+         x = np.zeros(sigma)
+         x[np.random.randint(sigma) % sigma] = 1
+         stxt = sample(G, x)
+         print ''.join([idx_to_char[x] for x in stxt])
+
+      # Prepare for next iteration
+      p += seq_length
+      n += 1
+
+
+if __name__ == '__main__':
+   main(sys.argv[1])
+
+
