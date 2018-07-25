98830b3bc464eb8364614ad6b9e7c0e4fde8d12e
gui11aume <guillaume.filion@gmail.com>
2018-07-25 09:05:47 +0200

    Working implementation with 3 layers.

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+##!/usr/bin/env python
 # -*- coding:utf-8 -*-
 
 import numpy as np
@@ -10,13 +10,18 @@ def onehot(sz, pos):
    return o
 
 
-class loss:
+class xent_loss:
    @staticmethod
    def fn(x,y):
-      return - np.log(np.dot(softmax.fn(x), y))
+      return - np.dot(x, np.log(y))
    @staticmethod
-   def dx(x,y):
-      return softmax.fn(x) - y
+   def dy(x,y):
+      # Here 'y' is a probability.
+      return - np.divide(x, y)
+   @staticmethod
+   def softmax_dy(x, y):
+      # Here 'y' is an energy.
+      return y - x
       
 class softmax:
    @staticmethod
@@ -24,12 +29,9 @@ class softmax:
       e = np.exp(x - np.max(x))
       return e / e.sum()
    @staticmethod
-   def df(x):
-      # TODO: test this bit (it may not be correct).
-      n = len(x)
-      M1 = np.repeat(x,n).reshape(n,n)
-      M2 = np.diag(1-2*np.diag(M1)) + M1
-      return np.multiply(M2, M1.T)
+   def jac(x):
+      tmp = x.reshape((-1,1))
+      return np.diag(x) - np.outer(tmp, tmp.T)
 
 class sigmoid:
    @staticmethod
@@ -48,21 +50,50 @@ class tanh:
       return 1 - x**2
 
 
-class GRULayer:
+class FwdParams:
+   def __init__(self, x = [], h = []):
+      self.x  =  x    # Input.
+      self.y  = [ ]   # Output.
+      self.z  = [ ]   # Update.
+      self.r  = [ ]   # Reset
+      self.hh = [ ]   # Hidden (hat).
+      self.h  =  h    # Hidden.
 
+   def apply_softmax(self):
+      self.y  = [softmax.fn(x) for x in self.y]
+      return self
+
+
+class GradParams:
+   def __init__(self, xsz, hsz, ysz):
+      self.dWz = np.zeros((hsz, xsz))
+      self.dWr = np.zeros((hsz, xsz))
+      self.dWh = np.zeros((hsz, xsz))
+      self.dWy = np.zeros((ysz, hsz))
+
+      self.dUz = np.zeros((hsz, hsz))
+      self.dUr = np.zeros((hsz, hsz))
+      self.dUh = np.zeros((hsz, hsz))
+
+      self.dbz = np.zeros(hsz)
+      self.dbr = np.zeros(hsz)
+      self.dbh = np.zeros(hsz)
+      self.dby = np.zeros(ysz)
+   
+   def apply_softmax_grad(self, L):
+      self.dx = [np.dot(softmax.jac(x), y) for (x,y) in zip(L, self.dx)]
+      return self
+
+
+
+class GRULayer:
    def __init__(self, xsz, hsz, ysz):
       # Dimensions.
       self.xsz = xsz
       self.hsz = hsz
       self.ysz = ysz
 
-      # Hidden states through time.
-      self.x  = list()           # Input vector.
-      self.z  = list()           # Update gate.
-      self.r  = list()           # Reset gate.
-      self.hh = list()           # Hidden intermediate.
-      self.h  = [np.zeros(hsz)]  # Hidden state.
-      self.y  = list()           # Outputs.
+      self.h  = np.zeros(hsz)  # Hidden state.
 
       # Parameters.
       self.Wz = np.random.rand(hsz, xsz) * 0.1 - 0.05
@@ -80,10 +111,6 @@ class GRULayer:
       self.Wy = np.random.rand(ysz, hsz) * 0.1 - 0.05
       self.by = np.zeros(ysz) 
 
-      # Gradients.
-      self.zerograd()
-      self.dhn = np.zeros(self.hsz)
-
       # Adagrad parameters.
       self.mdWz = np.zeros_like(self.Wz)
       self.mdWr = np.zeros_like(self.Wr)
@@ -100,134 +127,114 @@ class GRULayer:
       self.mdby = np.zeros_like(self.by)
 
 
-   def flush(self):
-      self.h  = [np.zeros(self.hsz)]
-      self.dhn = np.zeros(self.hsz)
+   def get_h(self):
+      return self.h
 
-
-   def zerograd(self):
-      self.dWz = np.zeros_like(self.Wz)
-      self.dWr = np.zeros_like(self.Wr)
-      self.dWh = np.zeros_like(self.Wh)
-      self.dWy = np.zeros_like(self.Wy)
-
-      self.dUz = np.zeros_like(self.Uz)
-      self.dUr = np.zeros_like(self.Ur)
-      self.dUh = np.zeros_like(self.Uh)
-
-      self.dbz = np.zeros_like(self.bz)
-      self.dbr = np.zeros_like(self.br)
-      self.dbh = np.zeros_like(self.bh)
-      self.dby = np.zeros_like(self.by)
-
-
-   def get_state(self):
-      return self.h[:]
-
-
-   def set_state(self, h):
+   def set_h(self, h):
       self.h = h
+
+   def reset(self):
+      self.h  = np.zeros(self.hsz)
+
 
 
    def forward(self, x):
-      # Update and reset gates.
-      z = sigmoid.fn(np.dot(self.Wz, x) + \
-            np.dot(self.Uz, self.h[-1]) + self.bz)
-      r = sigmoid.fn(np.dot(self.Wr, x) + \
-            np.dot(self.Ur, self.h[-1]) + self.br)
+      '''The parameter x is a list of 'np.array'. Outputs
+      a 'FwdParams' object.'''
 
-      # GRU units.
-      hh = tanh.fn(np.dot(self.Wh, x) + \
-            np.dot(self.Uh, np.multiply(r, self.h[-1])) + self.bh)
-      h = np.multiply(z, self.h[-1]) + np.multiply((1-z), hh)
-      y = np.dot(self.Wy, h) + self.by
+      fwd = FwdParams(x = x[:], h = [self.h])
+      prevh = self.h
 
-      # Write internal states.
-      self.x.append(x)
-      self.z.append(z)
-      self.r.append(r)
-      self.hh.append(hh)
-      self.y.append(y)
-      self.h.append(h)
+      for t in range(len(x)):
+         # Equations below are the definition of GRU.
+         z = sigmoid.fn(np.dot(self.Wz, x[t]) + \
+               np.dot(self.Uz, prevh) + self.bz)
+         r = sigmoid.fn(np.dot(self.Wr, x[t]) + \
+               np.dot(self.Ur, prevh) + self.br)
+         hh = tanh.fn(np.dot(self.Wh, x[t]) + \
+               np.dot(self.Uh, np.multiply(r, prevh)) + self.bh)
+         h = np.multiply(z, prevh) + np.multiply((1-z), hh)
+         y = np.dot(self.Wy, h) + self.by
 
-      return y
+         fwd.z.append(z)
+         fwd.r.append(r)
+         fwd.hh.append(hh)
+         fwd.h.append(h)
+         fwd.y.append(y)
+
+         prevh = h
+
+      self.h = h
+
+      # Return 'FwdParams' object.
+      return fwd
 
 
-   def backprop(self, dy):
-      '''The parameter 'dy' must be a list of gradients fed
-      into the GRU layer.'''
+   def backprop(self, fwd, dy):
+      '''dy is a list of gradients in output of the GRU layer.'''
 
-      self.zerograd()
+      grd = GradParams(self.xsz, self.hsz, self.ysz)
+      grd.dx = [None] * len(dy)
+
       dhn = np.zeros(self.hsz)
 
-      T = len(dy)
-      dx = list()
-
-      for t in range(T-1,-1,-1):
+      for t in reversed(range(len(dy))):
          # Layer output.
-         self.dby += dy[t]
-         self.dWy += np.outer(dy[t], self.h[t+1])
+         grd.dby += dy[t]
+         grd.dWy += np.outer(dy[t], fwd.h[t+1])
 
          # Intermediates.
          dh   = np.dot(self.Wy.T, dy[t]) + dhn
-         dhh  = np.multiply(dh, (1-self.z[t]))
-         dhhl = np.multiply(dhh, tanh.df(self.hh[t]))
+         dhh  = np.multiply(dh, (1-fwd.z[t]))
+         dhhl = np.multiply(dhh, tanh.df(fwd.hh[t]))
 
          # Gradient for 'hhat' parameters.
-         self.dWh += np.outer(dhhl, self.x[t])
-         self.dUh += np.outer(dhhl, np.multiply(self.r[t], self.h[t]))
-         self.dbh += dhhl
+         grd.dWh += np.outer(dhhl, fwd.x[t])
+         grd.dUh += np.outer(dhhl, np.multiply(fwd.r[t], fwd.h[t]))
+         grd.dbh += dhhl
 
          # Intermediates
          drh = np.dot(self.Uh.T, dhhl)
-         dr  = np.multiply(drh, self.h[t])
-         drl = dr * sigmoid.df(self.r[t])
+         dr  = np.multiply(drh, fwd.h[t])
+         drl = dr * sigmoid.df(fwd.r[t])
 
          # Gradient for 'r' parameters.
-         self.dWr += np.outer(drl, self.x[t])
-         self.dUr += np.outer(drl, self.h[t])
-         self.dbr += drl
+         grd.dWr += np.outer(drl, fwd.x[t])
+         grd.dUr += np.outer(drl, fwd.h[t])
+         grd.dbr += drl
 
          # Intermediates
-         dz  = np.multiply(dh, self.h[t] - self.hh[t])
-         dzl = dz * sigmoid.df(self.z[t])
+         dz  = np.multiply(dh, fwd.h[t] - fwd.hh[t])
+         dzl = dz * sigmoid.df(fwd.z[t])
 
          # Gradient for 'z' parameters.
-         self.dWz += np.outer(dzl, self.x[t])
-         self.dUz += np.outer(dzl, self.h[t].T)
-         self.dbz += dzl
+         grd.dWz += np.outer(dzl, fwd.x[t])
+         grd.dUz += np.outer(dzl, fwd.h[t].T)
+         grd.dbz += dzl
 
          # Gradient for next 'h'
          dh1 = np.dot(self.Uz.T, dzl)
          dh2 = np.dot(self.Ur.T, drl)
-         dh3 = np.multiply(dh, self.z[t])
-         dh4 = np.multiply(drh, self.r[t])
+         dh3 = np.multiply(dh, fwd.z[t])
+         dh4 = np.multiply(drh, fwd.r[t])
 
          dhn = dh1 + dh2 + dh3 + dh4
 
          # Send down the gradient.
          dxr = np.dot(self.Wr.T, drl)
          dxz = np.dot(self.Wz.T, dzl)
-         dx.append(dxr+dxz)
+         grd.dx[t] = dxr + dxz
 
-      # Erase internal variables.
-      self.x  = list()
-      self.z  = list()
-      self.r  = list()
-      self.hh = list()
-      self.h  = self.h[-1:]
-      self.y  = list()
-
-      return list(reversed(dx))
+      return grd
 
 
    # TODO: write this is a cleaner way.
-   def update(self, learning_rate=0.02):
+   def update(self, grd, learning_rate=0.02):
       # Update model with adagrad (stochastic) gradient descent
       plist = [self.Wy, self.Wh, self.Wr, self.Wz, self.Uh,
          self.Ur, self.Uz, self.by, self.bh, self.br, self.bz]
-      dlist = [self.dWy, self.dWh, self.dWr, self.dWz, self.dUh,
-         self.dUr, self.dUz, self.dby, self.dbh, self.dbr, self.dbz]
+      dlist = [grd.dWy, grd.dWh, grd.dWr, grd.dWz, grd.dUh,
+         grd.dUr, grd.dUz, grd.dby, grd.dbh, grd.dbr, grd.dbz]
       mlist = [self.mdWy, self.mdWh, self.mdWr, self.mdWz, self.mdUh,
          self.mdUr, self.mdUz, self.mdby, self.mdbh, self.mdbr, self.mdbz]
       for i in range(len(plist)):
@@ -238,23 +245,26 @@ class GRULayer:
       return
 
 
-def sample(G1, G2, x):
+def sample(G1, G2, G3, x):
    charidx = list()
-   init1 = G1.get_state()
-   init2 = G2.get_state()
-   for t in range(1000):
-      # Two-layer version.
-      #p = softmax.fn(G2.forward(G1.forward(x)))
-      # One-layer version.
+   initl1 = G1.get_h()
+   initl2 = G2.get_h()
+   initl3 = G3.get_h()
+   for t in range(4100):
+      # Three-layer version.
+      oG1 = G1.forward([x])
+      oG2 = G2.forward(oG1.y)
+      oG3 = G3.forward(oG2.y).apply_softmax()
+      (p,) = oG3.y
       # Choose next char according to the distribution
-      p = softmax.fn(G1.forward(x))
       idx = np.random.choice(range(len(x)), p=p.ravel())
       x = np.zeros_like(x)
       x[idx] = 1
       charidx.append(idx)
-   G1.set_state(init1)
-   G2.set_state(init2)
-   return charidx
+   G1.set_h(initl1)
+   G2.set_h(initl2)
+   G3.set_h(initl3)
+   return charidx[100:]
 
 
 
@@ -274,47 +284,138 @@ def main(fname):
 
    G1 = GRULayer(sigma, sigma, sigma)
    G2 = GRULayer(sigma, sigma, sigma)
+   G3 = GRULayer(sigma, sigma, sigma)
 
    seq_length = 25
    Si = range(seq_length)
-   learning_rate = 1e-1
-   print_interval = 1000
+   print_interval = 200
 
+
+   # Pre-training 1
    p = 1
-   n = 0
    seq = [onehot(sigma, char_to_idx[txt[0]])]
-   while True:
+   for n in range(50000):
       # Check if end of text is reached.
       if p + seq_length > txtlen:
-         G.flush()
+         G1.reset()
+         G2.reset()
+         G3.reset()
+         p = 1
          seq = [onehot(sigma, char_to_idx[txt[0]])]
          
       # Get input and target sequence
       for i in range(p, p+seq_length):
          seq.append(onehot(sigma, char_to_idx[txt[i]]))
       
-      # One-layer version.
-      oG1 = [G1.forward(seq[i]) for i in Si]
-      dG1 = G1.backprop([loss.dx(oG1[i], seq[i+1]) for i in Si])
-      G1.update()
+      # Three-layer version.
+      oG1 = G1.forward(seq[:-1]).apply_softmax()
 
-      # Two-layer version.
-      #oG1 = [G1.forward(seq[i]) for i in Si]
-      #oG2 = [G2.forward(oG1[i]) for i in Si]
-      #dG2 = G2.backprop([loss.dx(oG2[i], seq[i+1]) for i in Si])
-      #dG1 = G1.backprop(dG2)
-      #G1.update()
-      #G2.update()
+      loss_grad = [xent_loss.softmax_dy(seq[i], oG1.y[i]) for i in Si]
+      dG1 = G1.backprop(oG1, loss_grad)
+
+      G1.update(dG1)
 
       if n % print_interval == 0:
-         # Two-layer version.
-         #losses = sum([loss.fn(oG2[i], seq[i+1]) for i in Si])
-         # One-layer version.
-         losses = sum([loss.fn(oG1[i], seq[i+1]) for i in Si])
-         print '\niteration: %d, loss: %f\n' % (n, losses)
+         # Three-layer version.
+         #losses = sum([xent_loss.fn(seq[i+1], oG3.y[i]) for i in Si])
+         losses = sum([xent_loss.fn(seq[i], oG1.y[i]) for i in Si])
+         print 'iteration: %d, loss: %f' % (n, losses)
          x = np.zeros(sigma)
          x[np.random.randint(sigma) % sigma] = 1
-         stxt = sample(G1, G2, x)
+         #stxt = sample(G1, G2, G3, x)
+         #print ''.join([idx_to_char[x] for x in stxt])
+
+      # Keep last character for next round.
+      seq = seq[-1:]
+
+      # Prepare for next iteration
+      p += seq_length
+      n += 1
+
+   # Pre-training 1+2
+   seq = [onehot(sigma, char_to_idx[txt[0]])]
+   for n in range(50000):
+      # Check if end of text is reached.
+      if p + seq_length > txtlen:
+         G1.reset()
+         G2.reset()
+         G3.reset()
+         p = 1
+         seq = [onehot(sigma, char_to_idx[txt[0]])]
+         
+      # Get input and target sequence
+      for i in range(p, p+seq_length):
+         seq.append(onehot(sigma, char_to_idx[txt[i]]))
+      
+      # Three-layer version.
+      oG1 = G1.forward(seq[:-1])
+      oG2 = G2.forward(oG1.y).apply_softmax()
+
+      loss_grad = [xent_loss.softmax_dy(seq[i], oG2.y[i]) for i in Si]
+      dG2 = G1.backprop(oG2, loss_grad)
+      dG1 = G1.backprop(oG1, dG2.dx)
+
+      G1.update(dG1)
+      G2.update(dG2)
+
+      if n % print_interval == 0:
+         # Three-layer version.
+         #losses = sum([xent_loss.fn(seq[i+1], oG3.y[i]) for i in Si])
+         losses = sum([xent_loss.fn(seq[i], oG2.y[i]) for i in Si])
+         print 'iteration: %d, loss: %f' % (n, losses)
+         x = np.zeros(sigma)
+         x[np.random.randint(sigma) % sigma] = 1
+         #stxt = sample(G1, G2, G3, x)
+         #print ''.join([idx_to_char[x] for x in stxt])
+
+      # Keep last character for next round.
+      seq = seq[-1:]
+
+      # Prepare for next iteration
+      p += seq_length
+      n += 1
+
+
+   # Training proper.
+   print_interval = 10000
+   p = 1
+   n = 0
+   seq = [onehot(sigma, char_to_idx[txt[0]])]
+   while True:
+      # Check if end of text is reached.
+      if p + seq_length > txtlen:
+         G1.reset()
+         G2.reset()
+         G3.reset()
+         p = 1
+         seq = [onehot(sigma, char_to_idx[txt[0]])]
+         
+      # Get input and target sequence
+      for i in range(p, p+seq_length):
+         seq.append(onehot(sigma, char_to_idx[txt[i]]))
+      
+      # Three-layer version.
+      oG1 = G1.forward(seq[:-1])
+      oG2 = G2.forward(oG1.y)
+      oG3 = G3.forward(oG2.y).apply_softmax()
+
+
+      loss_grad = [xent_loss.softmax_dy(seq[i+1], oG3.y[i]) for i in Si]
+      dG3 = G3.backprop(oG3, loss_grad)
+      dG2 = G2.backprop(oG2, dG3.dx)
+      dG1 = G1.backprop(oG1, dG2.dx)
+
+      G1.update(dG1)
+      G2.update(dG2)
+      G3.update(dG3)
+
+      if n % print_interval == 0:
+         # Three-layer version.
+         losses = sum([xent_loss.fn(seq[i+1], oG3.y[i]) for i in Si])
+         print 'iteration: %d, loss: %f' % (n, losses)
+         x = np.zeros(sigma)
+         x[np.random.randint(sigma) % sigma] = 1
+         stxt = sample(G1, G2, G3, x)
          print ''.join([idx_to_char[x] for x in stxt])
 
       # Keep last character for next round.
